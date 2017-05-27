82c41ed95a84a18f0c7f0589c1df16e93bd0cd3c
favsol26 <favsol26@gmail.com>
2017-05-27 13:20:27 -0400

    changes completes

--- app/src/main/java/com/example/faustinoayala/layouts/MainActivity.java
+++ app/src/main/java/com/example/faustinoayala/layouts/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.faustinoayala.layouts;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.scrolliview);
+        LinearLayout corre = (LinearLayout) findViewById(R.id.button_container);
+        for (int i = 0; i < 15; i++) {
+            Button btn = new Button(this);
+            btn.setText(getString(R.string.button) + " "+(i+1));
+            corre.addView(btn);
+        }
     }
+
+
 }
