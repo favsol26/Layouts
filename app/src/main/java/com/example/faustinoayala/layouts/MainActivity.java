ea2c07f6f83a938aa28d86ce50af0e7c69957aba
favsol26 <favsol26@gmail.com>
2017-05-27 13:39:26 -0400

    changes completes

--- app/src/main/java/com/example/faustinoayala/layouts/MainActivity.java
+++ app/src/main/java/com/example/faustinoayala/layouts/MainActivity.java
@@ -12,9 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scrolliview);
+
         LinearLayout run = (LinearLayout) findViewById(R.id.button_container);
+
         for (int i = 0; i < 15; i++) {
             Button btn = new Button(this);
+
             btn.setText(getString(R.string.button) + " " + (i + 1));
             run.addView(btn);
         }
